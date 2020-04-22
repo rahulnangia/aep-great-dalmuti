2586c9ba58470f84f965784ed21935b0d9d86340
Rahul <rahul@Rahuls-MacBook-Pro.local>
2020-04-22 02:04:55 -0700

    fixed error in commit: added functionality of distributing cards and its tests

--- src/main/java/edu/berkeley/aep/dalmuti/Game.java
+++ src/main/java/edu/berkeley/aep/dalmuti/Game.java
@@ -1,6 +1,5 @@
 package edu.berkeley.aep.dalmuti;
 
-import edu.berkeley.aep.dalmuti.exceptions.GameAlreadyFullException;
 
 import java.util.*;
 
@@ -45,10 +44,7 @@ public class Game {
      * @return
      */
     public boolean registerPlayer(Player player) {
-        if(this.players.size() == 8){
-            throw new GameAlreadyFullException();
-        }
-        if (this.players.add(player)) {
+       if (this.players.add(player)) {
             playingOrder.add(player);
             return true;
         }
