bcca05121863650a5d30ede0775258851b9f7f69
Rahul <rahul@Rahuls-MacBook-Pro.local>
2020-04-22 00:10:01 -0700

    added functionality to assign the playing order for the game

--- src/main/java/edu/berkeley/aep/dalmuti/Game.java
+++ src/main/java/edu/berkeley/aep/dalmuti/Game.java
@@ -3,21 +3,59 @@ package edu.berkeley.aep.dalmuti;
 import java.util.*;
 
 /**
+ * A class that ndertands the current Game, by the deck and the current players
  * @Author rahul
  * @Create Apr 2020
  */
 public class Game {
+
+    /**
+     * Maintains a set of unique players
+     */
     private final Set<Player> players;
+
+    /**
+     * Random generator for the Game
+     */
+    private final Random randomizer;
+
+    /**
+     * Contains the current playing order
+     */
+    private List<Player> playingOrder;
 
     public Game() {
         this.players = new HashSet<>();
+        this.playingOrder = new ArrayList<>();
+        this.randomizer = new Random(System.currentTimeMillis());
     }
 
+    /**
+     * Registers a player in the game and returns True if successful otherwise False
+     * @param player
+     * @return
+     */
     public boolean registerPlayer(Player player) {
-        return this.players.add(player);
+        if(this.players.add(player)){
+            playingOrder.add(player);
+            return true;
+        }
+        return false;
     }
 
+    /**
+     * Gives the current playing order of the Game
+     * @return
+     */
     public List<Player> getCurrentPlayers() {
-        return new LinkedList<>(players);
+        return playingOrder;
+    }
+
+    /**
+     * Method used to assign a playing order for the Game
+     * @return
+     */
+    public void assignPlayingOrder() {
+        Collections.shuffle(playingOrder, randomizer);
     }
 }
--- src/test/java/edu/berkeley/aep/dalmuti/GameTest.java
+++ src/test/java/edu/berkeley/aep/dalmuti/GameTest.java
@@ -2,6 +2,10 @@ package edu.berkeley.aep.dalmuti;
 
 import org.junit.Test;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,7 +15,7 @@ import static org.junit.Assert.*;
 public class GameTest {
 
     @Test
-    public void registerPlayerShouldAddPlayerToGamePlayers(){
+    public void registerPlayerShouldAddPlayerToGamePlayers() {
         Player player = new Player("Rahul", 22);
         Game newGame = new Game();
         assertTrue(newGame.registerPlayer(player));
@@ -19,25 +23,62 @@ public class GameTest {
     }
 
     @Test
-    public void playersWithSameNameLuckyNosShouldBeAbleToRegister(){
+    public void playersWithSameNameLuckyNosShouldBeAbleToRegister() {
         Player player = new Player("Rahul", 22);
         Player anotherPlayer = new Player("Rahul", 22);
         Game newGame = new Game();
+//        Add the players
         assertTrue(newGame.registerPlayer(player));
         assertTrue(newGame.registerPlayer(anotherPlayer));
+//        check if added
         assertEquals(2, newGame.getCurrentPlayers().size());
         assertTrue(newGame.getCurrentPlayers().contains(player));
         assertTrue(newGame.getCurrentPlayers().contains(anotherPlayer));
     }
 
     @Test
-    public void registeringSamePlayerTwiceDoesNotAddThemtoGame(){
+    public void registeringSamePlayerTwiceDoesNotAddThemToGame() {
         Player player = new Player("Rahul", 22);
         Game newGame = new Game();
         assertTrue(newGame.registerPlayer(player));
         assertFalse(newGame.registerPlayer(player));
         assertEquals(1, newGame.getCurrentPlayers().size());
         assertTrue(newGame.getCurrentPlayers().contains(player));
+    }
+
+    @Test
+    public void playersShouldBePlacedInADifferentOrderThanRegistering() {
+        int count = 10;
+        List<Player> players = initializeNPlayers(count);
+        Game newGame = new Game();
+        for (Player player : players) {
+            newGame.registerPlayer(player);
+        }
+        newGame.assignPlayingOrder();
+        Iterator<Player> playingOrderItr = newGame.getCurrentPlayers().iterator();
+        int mismatchCount = 0;
+        for (Player originalOrderPlayer : players) {
+            Player playerForPlayingOrder = playingOrderItr.next();
+//            Check if current player in playing order belongs to original but is not at the same place
+            if (players.contains(playerForPlayingOrder) && originalOrderPlayer != playerForPlayingOrder) {
+                mismatchCount++;
+            }
+        }
+        assertTrue(mismatchCount > 0);
+    }
+
+    /**
+     * Initializes N players and returns.
+     *
+     * @param count
+     * @return
+     */
+    public List<Player> initializeNPlayers(int count) {
+        List<Player> list = new LinkedList<>();
+        for (int i = 0; i < count; i++) {
+            list.add(new Player("abc", 10));
+        }
+        return list;
     }
 
 }
