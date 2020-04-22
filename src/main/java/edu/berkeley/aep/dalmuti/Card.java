089cbb63920f555df7d34edcd6c217b48464aad0
Rahul <rahul@Rahuls-MacBook-Pro.local>
2020-04-22 01:35:26 -0700

    defined the playing card and created the card deck

---
+++ src/main/java/edu/berkeley/aep/dalmuti/Card.java
@@ -0,0 +1,54 @@
+package edu.berkeley.aep.dalmuti;
+
+import java.util.*;
+
+/**
+ * A card that understands a Playing card containing just number rank and no suits
+ * @Author rahul
+ * @Create Apr 2020
+ */
+public class Card {
+
+    private static final List<Card> CARD_DECK;
+
+    static {
+        CARD_DECK = new LinkedList<>();
+        for(int card = 1; card<13;card++){
+            for(int j=0;j<card;j++) {
+                CARD_DECK.add(new Card(card));
+            }
+        }
+        //Add 1 Great Dalmuti
+        CARD_DECK.add(new Card(0));
+        //Add two jesters
+        CARD_DECK.add(new Card(13));
+        CARD_DECK.add(new Card(13));
+    }
+
+    public static List<Card> getACardDeck() {
+        return new LinkedList<>(CARD_DECK);
+    }
+
+    private final int rank;
+
+    public Card(int rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Card)) return false;
+        Card card = (Card) o;
+        return rank == card.rank;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rank);
+    }
+
+    public int getRank() {
+        return rank;
+    }
+}
---
+++ src/test/java/edu/berkeley/aep/dalmuti/CardTest.java
@@ -0,0 +1,8 @@
+package edu.berkeley.aep.dalmuti;
+
+/**
+ * @Author rahul
+ * @Create Apr 2020
+ */
+public class CardTest {
+}
